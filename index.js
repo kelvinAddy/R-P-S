f0f78ba7dcc86bbf390b9254a10aae5bdcfb8a53
Kelvin Addy <Keladdy2131@gmail.com>
2025-08-05 19:19:25 +0000

    Completed game logic

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const gameBtns = document.querySelectorAll(".choice");
-const playBtn = document.querySelector(".play-button");
 
 const playerState = document.querySelector(".player");
 const compState = document.querySelector(".computer");
 
-let playerChoice = "";
+const playerCurrentScore = document.getElementById("player-score");
+const computerCurrentScore = document.getElementById("computer-score");
+
+const seriesCount = document.getElementById("best-of");
+
+const gameHeader = document.querySelector(".game-header-label");
+const restartBtn = document.querySelector(".restart");
 
 let playerScore = 0;
 let compScore = 0;
+let count = 3;
+let seriesNum = seriesCount.value;
+
+let playerWin = false;
+let stalemate = false;
+
+let playerChoice;
+let computerChoice;
+
+const [ROCK, PAPER, SCISSORS] = ["ROCK", "PAPER", "SCISSORS"];
 
 const gameChoices = {
-  rock: 0,
-  paper: 1,
-  scissors: 2,
+  ROCK: 0,
+  PAPER: 1,
+  SCISSORS: 2,
 };
 
 const getComputerChoice = () => {
@@ -22,36 +37,133 @@ const getComputerChoice = () => {
   }
 };
 
-let computerChoice = getComputerChoice();
-
-const matchWinner = () => {
-  if (computerChoice === playerChoice) {
-    // draw scenario triggered
-  } else if (playerChoice === "rock") {
-    if (computerChoice === "scissors") {
-      // player wins
-    } else {
-      // computer wins
-    }
-  } else if (playerChoice === "scissors") {
-    if (computerChoice === "rock") {
-      // computer wins
-    } else {
-      // player wins
-    }
-  } else if (playerChoice === "paper") {
-    if (computerChoice === "rock") {
-      // player wins
-    } else {
-      // computer wins
-    }
-  }
-  updateScore();
+const clearContent = (node) => {
+  node.textContent = "";
 };
 
-const updateScore = (playerWin, stalemate) => {
+const resetGame = () => {
+  playerWin = false;
+  stalemate = false;
+  playerScore = 0;
+  compScore = 0;
+  seriesNum = seriesCount.value;
+  clearContent(playerState);
+  clearContent(compState);
+  gameHeader.textContent = "ROCK PAPER SCISSORS";
+  playerCurrentScore.style.color = "#555";
+  computerCurrentScore.style.color = "#555";
+  computerCurrentScore.textContent = compScore;
+  playerCurrentScore.textContent = playerScore;
+  document.body.classList.remove("win-bg", "lose-bg", "draw-bg");
+  toogleBtn(false);
+};
+
+const toogleBtn = (value) => {
+  gameBtns.forEach((x) => {
+    x.disabled = value;
+  });
+  seriesCount.disabled = value;
+  restartBtn.disabled = value;
+};
+
+const updateWinnerLabel = () => {
   if (!stalemate) {
-    if (playerWin) playerScore++;
-    else compScore++;
+    gameHeader.textContent = playerWin ? "YOU WON!!" : "COMPUTER WON!!";
+    playerCurrentScore.style.color = playerWin ? "green" : "red";
+    computerCurrentScore.style.color = !playerWin ? "green" : "red";
+  } else {
+    gameHeader.textContent = "STALEMATE!!";
+    playerCurrentScore.style.color = "black";
+    computerCurrentScore.style.color = "black";
   }
 };
+
+const displayImage = (image, player, playerStr) => {
+  let node = document.createElement("img");
+  node.classList.add("game-img");
+  node.src = `./Assets/${playerStr}/${image}.png`;
+  clearContent(player);
+  player.appendChild(node);
+};
+
+const updateScore = () => {
+  if (!stalemate) {
+    if (playerWin) {
+      playerScore++;
+      playerCurrentScore.textContent = playerScore;
+    } else {
+      compScore++;
+      computerCurrentScore.textContent = compScore;
+    }
+  }
+};
+
+const checkGameOver = () => {
+  if (seriesNum === 0) {
+    toogleBtn(true);
+    if (playerScore !== compScore) {
+      console.log("trial");
+      const isGameWon = playerScore > compScore;
+      gameHeader.textContent = isGameWon
+        ? "YOU WON THE GAME!!!"
+        : "YOU LOST THE GAME!!!";
+      document.body.classList.add(isGameWon ? "win-bg" : "lose-bg");
+    } else {
+      console.log("test");
+      gameHeader.textContent = "YOU DREW THE GAME!!!";
+      document.body.classList.add("draw-bg");
+    }
+  }
+  restartBtn.disabled = false;
+};
+const matchWinner = () => {
+  let timer = setInterval(() => {
+    if (count === 0) {
+      clearContent(compState);
+      clearInterval(timer);
+      if (computerChoice === playerChoice) {
+        stalemate = true;
+      } else if (playerChoice === ROCK) {
+        stalemate = false;
+        playerWin = computerChoice === SCISSORS ? true : false;
+      } else if (playerChoice === SCISSORS) {
+        stalemate = false;
+        playerWin = computerChoice === ROCK ? false : true;
+      } else if (playerChoice === PAPER) {
+        stalemate = false;
+        playerWin = computerChoice === ROCK ? true : false;
+      }
+      updateScore();
+      displayImage(computerChoice, compState, "computer");
+      updateWinnerLabel();
+      toogleBtn(false);
+      checkGameOver();
+    } else {
+      toogleBtn(true);
+      compState.textContent = count;
+      count -= 1;
+      gameHeader.textContent = "Thinking....";
+    }
+  }, 500);
+  count = 3;
+};
+
+gameBtns.forEach((x) => {
+  x.addEventListener("click", (e) => {
+    playerChoice = e.target.textContent;
+    computerChoice = getComputerChoice();
+    matchWinner();
+    displayImage(playerChoice, playerState, "player");
+    seriesNum--;
+    console.log(seriesNum);
+  });
+});
+
+seriesCount.addEventListener("change", () => {
+  seriesNum = seriesCount.value;
+  resetGame();
+});
+
+restartBtn.addEventListener("click", () => {
+  resetGame();
+});
